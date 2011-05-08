293602d787d9fa29addf8d29369db990abd540e3
nickp10 <nickp10@hotmail.com>
2011-05-08 00:14:08 +0000

    Adding some multiple instances in the play mode tests as well as test for the moveWithinQueue method. Also added some test utilities.

--- mCubedTest/src/dev/paddock/adp/mCubed/model/PlayModeTest.java
+++ mCubedTest/src/dev/paddock/adp/mCubed/model/PlayModeTest.java
@@ -13,6 +13,26 @@ public class PlayModeTest extends AndroidTestCase{
 	private final List<MediaFile> files = new ArrayList<MediaFile>();
 	private Playlist playlist;
 	
+	public static <T> void assertSequenceEmpty(List<T> actual) {
+		if (actual == null) {
+			assertNull(actual);
+		} else {
+			assertEquals(0, actual.size());
+		}
+	}
+	
+	public static <T> void assertSequenceEquals(T[] expected, List<T> actual) {
+		if (expected == null || actual == null) {
+			assertNull(expected);
+			assertNull(actual);
+		} else {
+			assertEquals(expected.length, actual.size());
+			for (int i = 0; i < expected.length; i++) {
+				assertEquals(expected[i], actual.get(i));
+			}
+		}
+	}
+	
 	@UsesMocks({ MediaFile.class, Playlist.class })
 	@Override
 	protected void setUp() throws Exception {
@@ -24,6 +44,7 @@ public class PlayModeTest extends AndroidTestCase{
 			AndroidMock.replay(file);
 			mediaFiles[i] = file;
 		}
+		files.clear();
 		playlist = AndroidMock.createMock(Playlist.class, true);
 		AndroidMock.expect(playlist.getFiles()).andReturn(files).anyTimes();
 		AndroidMock.replay(playlist);
@@ -182,6 +203,216 @@ public class PlayModeTest extends AndroidTestCase{
 		assertFalse(playMode.getCurrentRequiresRepeat());
 	}
 	
+	/**
+	 * The following tests are to test the removedFromPlaylist functionality
+	 * when one MediaFile instance appears multiple times. Here are all possibilities:
+	 * - The history contains the MediaFile two or more times (testRemovedFromPlaylist_MultipleInstanceInHistory)
+	 * - The queue contains the MediaFile two or more times (testRemovedFromPlaylist_MultipleInstanceInQueue)
+	 * - The history and queue both contain the MediaFile one or more times (testRemovedFromPlaylist_MultipleInstanceInHistoryAndQueue)
+	 * - The history contains the MediaFile one or more times, and it is the current MediaFile (testRemovedFromPlaylist_MultipleInstanceInHistoryAndIsCurrent)
+	 * - The queue contains the MediaFile one or more times, and it is the current MediaFile (testRemovedFromPlaylist_MultipleInstanceInQueueAndIsCurrent)
+	 * - The history and queue both contain the MediaFile one or more times, and it is the current MediaFile (testRemovedFromPlaylist_MultipleInstanceInHistoryAndQueueAndIsCurrent)
+	 */
+	public void testRemovedFromPlaylist_MultipleInstanceInHistory() {
+		// Setup the play mode
+		PlayMode playMode = new PlayMode(playlist, false);
+		files.add(mediaFiles[0]);
+		playMode.addedToPlaylist(mediaFiles[0]);
+		playMode.appendToQueue(mediaFiles[0]);
+		playMode.appendToQueue(mediaFiles[0]);
+		playMode.appendToQueue(mediaFiles[0]);
+		playMode.appendToQueue(mediaFiles[0]);
+		files.add(mediaFiles[1]);
+		playMode.addedToPlaylist(mediaFiles[1]);
+		files.add(mediaFiles[2]);
+		playMode.addedToPlaylist(mediaFiles[2]);
+		playMode.next();
+		playMode.next();
+		playMode.next();
+		playMode.next();
+		playMode.next();
+		
+		// Assert the state before
+		assertSequenceEquals(new MediaFile[] { mediaFiles[0], mediaFiles[0], mediaFiles[0], mediaFiles[0], mediaFiles[0] }, playMode.getHistory());
+		assertEquals(mediaFiles[1], playMode.getCurrent());
+		assertSequenceEquals(new MediaFile[] { mediaFiles[2] }, playMode.getQueue());
+		
+		// Remove the instance
+		files.remove(mediaFiles[0]);
+		playMode.removedFromPlaylist(mediaFiles[0]);
+		
+		// Assert the state after
+		assertSequenceEmpty(playMode.getHistory());
+		assertEquals(mediaFiles[1], playMode.getCurrent());
+		assertSequenceEquals(new MediaFile[] { mediaFiles[2] }, playMode.getQueue());
+	}
+	
+	public void testRemovedFromPlaylist_MultipleInstanceInQueue() {
+		// Setup the play mode
+		PlayMode playMode = new PlayMode(playlist, false);
+		files.add(mediaFiles[0]);
+		playMode.addedToPlaylist(mediaFiles[0]);
+		files.add(mediaFiles[1]);
+		playMode.addedToPlaylist(mediaFiles[1]);
+		playMode.appendToQueue(mediaFiles[1]);
+		playMode.appendToQueue(mediaFiles[1]);
+		playMode.appendToQueue(mediaFiles[1]);
+		playMode.appendToQueue(mediaFiles[1]);
+		files.add(mediaFiles[2]);
+		playMode.addedToPlaylist(mediaFiles[2]);
+		
+		// Assert the state before
+		assertSequenceEmpty(playMode.getHistory());
+		assertEquals(mediaFiles[0], playMode.getCurrent());
+		assertSequenceEquals(new MediaFile[] { mediaFiles[1], mediaFiles[1], mediaFiles[1], mediaFiles[1], mediaFiles[1] }, playMode.getQueue());
+		
+		// Remove the instance
+		files.remove(mediaFiles[1]);
+		playMode.removedFromPlaylist(mediaFiles[1]);
+		
+		// Assert the state after
+		assertSequenceEmpty(playMode.getHistory());
+		assertEquals(mediaFiles[0], playMode.getCurrent());
+		assertSequenceEquals(new MediaFile[] { mediaFiles[2] }, playMode.getQueue());
+	}
+	
+	public void testRemovedFromPlaylist_MultipleInstanceInHistoryAndQueue() {
+		// Setup the play mode
+		PlayMode playMode = new PlayMode(playlist, false);
+		files.add(mediaFiles[0]);
+		playMode.addedToPlaylist(mediaFiles[0]);
+		playMode.appendToQueue(mediaFiles[0]);
+		playMode.appendToQueue(mediaFiles[0]);
+		playMode.appendToQueue(mediaFiles[0]);
+		files.add(mediaFiles[1]);
+		playMode.addedToPlaylist(mediaFiles[1]);
+		playMode.appendToQueue(mediaFiles[1]);
+		playMode.appendToQueue(mediaFiles[0]);
+		playMode.appendToQueue(mediaFiles[0]);
+		playMode.appendToQueue(mediaFiles[0]);
+		playMode.appendToQueue(mediaFiles[0]);
+		files.add(mediaFiles[2]);
+		playMode.addedToPlaylist(mediaFiles[2]);
+		playMode.next();
+		playMode.next();
+		playMode.next();
+		playMode.next();
+		
+		// Assert the state before
+		assertSequenceEquals(new MediaFile[] { mediaFiles[0], mediaFiles[0], mediaFiles[0], mediaFiles[0] }, playMode.getHistory());
+		assertEquals(mediaFiles[1], playMode.getCurrent());
+		assertSequenceEquals(new MediaFile[] { mediaFiles[0], mediaFiles[0], mediaFiles[0], mediaFiles[0] }, playMode.getQueue());
+		
+		// Remove the instance
+		files.remove(mediaFiles[0]);
+		playMode.removedFromPlaylist(mediaFiles[0]);
+		
+		// Assert the state after
+		assertSequenceEmpty(playMode.getHistory());
+		assertEquals(mediaFiles[1], playMode.getCurrent());
+		assertSequenceEquals(new MediaFile[] { mediaFiles[2] }, playMode.getQueue());
+	}
+	
+	public void testRemovedFromPlaylist_MultipleInstanceInHistoryAndIsCurrent() {
+		// Setup the play mode
+		PlayMode playMode = new PlayMode(playlist, false);
+		files.add(mediaFiles[0]);
+		playMode.addedToPlaylist(mediaFiles[0]);
+		playMode.appendToQueue(mediaFiles[0]);
+		playMode.appendToQueue(mediaFiles[0]);
+		playMode.appendToQueue(mediaFiles[0]);
+		playMode.appendToQueue(mediaFiles[0]);
+		files.add(mediaFiles[1]);
+		playMode.addedToPlaylist(mediaFiles[1]);
+		files.add(mediaFiles[2]);
+		playMode.addedToPlaylist(mediaFiles[2]);
+		playMode.next();
+		playMode.next();
+		playMode.next();
+		playMode.next();
+		
+		// Assert the state before
+		assertSequenceEquals(new MediaFile[] { mediaFiles[0], mediaFiles[0], mediaFiles[0], mediaFiles[0] }, playMode.getHistory());
+		assertEquals(mediaFiles[0], playMode.getCurrent());
+		assertSequenceEquals(new MediaFile[] { mediaFiles[1] }, playMode.getQueue());
+		
+		// Remove the instance
+		files.remove(mediaFiles[0]);
+		playMode.removedFromPlaylist(mediaFiles[0]);
+		
+		// Assert the state after
+		assertSequenceEmpty(playMode.getHistory());
+		assertEquals(mediaFiles[1], playMode.getCurrent());
+		assertSequenceEquals(new MediaFile[] { mediaFiles[2] }, playMode.getQueue());
+	}
+	
+	public void testRemovedFromPlaylist_MultipleInstanceInQueueAndIsCurrent() {
+		// Setup the play mode
+		PlayMode playMode = new PlayMode(playlist, false);
+		files.add(mediaFiles[0]);
+		playMode.addedToPlaylist(mediaFiles[0]);
+		files.add(mediaFiles[1]);
+		playMode.addedToPlaylist(mediaFiles[1]);
+		playMode.appendToQueue(mediaFiles[1]);
+		playMode.appendToQueue(mediaFiles[1]);
+		playMode.appendToQueue(mediaFiles[1]);
+		playMode.appendToQueue(mediaFiles[1]);
+		files.add(mediaFiles[2]);
+		playMode.addedToPlaylist(mediaFiles[2]);
+		playMode.next();
+		
+		// Assert the state before
+		assertSequenceEquals(new MediaFile[] { mediaFiles[0] }, playMode.getHistory());
+		assertEquals(mediaFiles[1], playMode.getCurrent());
+		assertSequenceEquals(new MediaFile[] { mediaFiles[1], mediaFiles[1], mediaFiles[1], mediaFiles[1] }, playMode.getQueue());
+		
+		// Remove the instance
+		files.remove(mediaFiles[1]);
+		playMode.removedFromPlaylist(mediaFiles[1]);
+		
+		// Assert the state after
+		assertSequenceEquals(new MediaFile[] { mediaFiles[0] }, playMode.getHistory());
+		assertEquals(mediaFiles[2], playMode.getCurrent());
+		assertSequenceEmpty(playMode.getQueue());
+	}
+	
+	public void testRemovedFromPlaylist_MultipleInstanceInHistoryAndQueueAndIsCurrent() {
+		// Setup the play mode
+		PlayMode playMode = new PlayMode(playlist, false);
+		files.add(mediaFiles[0]);
+		playMode.addedToPlaylist(mediaFiles[0]);
+		playMode.appendToQueue(mediaFiles[0]);
+		playMode.appendToQueue(mediaFiles[0]);
+		playMode.appendToQueue(mediaFiles[0]);
+		playMode.appendToQueue(mediaFiles[0]);
+		playMode.appendToQueue(mediaFiles[0]);
+		playMode.appendToQueue(mediaFiles[0]);
+		playMode.appendToQueue(mediaFiles[0]);
+		playMode.appendToQueue(mediaFiles[0]);
+		files.add(mediaFiles[1]);
+		playMode.addedToPlaylist(mediaFiles[1]);
+		files.add(mediaFiles[2]);
+		playMode.addedToPlaylist(mediaFiles[2]);
+		playMode.next();
+		playMode.next();
+		playMode.next();
+		playMode.next();
+		
+		// Assert the state before
+		assertSequenceEquals(new MediaFile[] { mediaFiles[0], mediaFiles[0], mediaFiles[0], mediaFiles[0] }, playMode.getHistory());
+		assertEquals(mediaFiles[0], playMode.getCurrent());
+		assertSequenceEquals(new MediaFile[] { mediaFiles[0], mediaFiles[0], mediaFiles[0], mediaFiles[0] }, playMode.getQueue());
+		
+		// Remove the instance
+		files.remove(mediaFiles[0]);
+		playMode.removedFromPlaylist(mediaFiles[0]);
+		
+		// Assert the state after
+		assertSequenceEmpty(playMode.getHistory());
+		assertEquals(mediaFiles[1], playMode.getCurrent());
+		assertSequenceEquals(new MediaFile[] { mediaFiles[2] }, playMode.getQueue());
+	}
+	
 	public void testNext() {
 		// Setup the play mode
 		PlayMode playMode = new PlayMode(playlist, false);
@@ -304,5 +535,62 @@ public class PlayModeTest extends AndroidTestCase{
 		assertEquals(mediaFiles[1], playMode.getQueue().get(0));
 		assertEquals(mediaFiles[2], playMode.getQueue().get(1));
 		assertFalse(playMode.getCurrentRequiresRepeat());
+	}
+	
+	public void testMoveWithinQueue() {
+		// Setup the play mode
+		PlayMode playMode = new PlayMode(playlist, false);
+		files.add(mediaFiles[0]);
+		playMode.addedToPlaylist(mediaFiles[0]);
+		files.add(mediaFiles[1]);
+		playMode.addedToPlaylist(mediaFiles[1]);
+		files.add(mediaFiles[2]);
+		playMode.addedToPlaylist(mediaFiles[2]);
+		playMode.appendToQueue(mediaFiles[0]);
+		playMode.appendToQueue(mediaFiles[0]);
+		playMode.appendToQueue(mediaFiles[2]);
+		playMode.appendToQueue(mediaFiles[2]);
+		playMode.appendToQueue(mediaFiles[1]);
+		
+		// Assert the setup
+		assertSequenceEquals(new MediaFile[] { mediaFiles[1], mediaFiles[0], mediaFiles[0], mediaFiles[2], mediaFiles[2], mediaFiles[1] }, playMode.getQueue());
+		
+		// Move file 0 to the end
+		playMode.moveWithinQueue(mediaFiles[0], 6);
+		
+		// Assert the move
+		assertSequenceEquals(new MediaFile[] { mediaFiles[1], mediaFiles[2], mediaFiles[2], mediaFiles[1], mediaFiles[0] }, playMode.getQueue());
+		
+		// Move file 2 to the beginning
+		playMode.moveWithinQueue(mediaFiles[2], 0);
+		
+		// Assert the move
+		assertSequenceEquals(new MediaFile[] { mediaFiles[2], mediaFiles[1], mediaFiles[1], mediaFiles[0] }, playMode.getQueue());
+		
+		// Move file 1 nowhere, but should remove the duplicate
+		playMode.moveWithinQueue(mediaFiles[1], 1);
+		
+		// Assert the move
+		assertSequenceEquals(new MediaFile[] { mediaFiles[2], mediaFiles[1], mediaFiles[0] }, playMode.getQueue());
+		
+		// Move file 1 nowhere, again
+		playMode.moveWithinQueue(mediaFiles[1], 1);
+		
+		// Assert the move
+		assertSequenceEquals(new MediaFile[] { mediaFiles[2], mediaFiles[1], mediaFiles[0] }, playMode.getQueue());
+		
+		// Move file 0 nowhere
+		playMode.moveWithinQueue(mediaFiles[0], 2);
+		
+		// Assert the move and setup again
+		assertSequenceEquals(new MediaFile[] { mediaFiles[2], mediaFiles[1], mediaFiles[0] }, playMode.getQueue());
+		playMode.appendToQueue(mediaFiles[2]);
+		assertSequenceEquals(new MediaFile[] { mediaFiles[2], mediaFiles[1], mediaFiles[0], mediaFiles[2] }, playMode.getQueue());
+		
+		// Move file 2 between files 1 and 0
+		playMode.moveWithinQueue(mediaFiles[2], 2);
+		
+		// Assert the move
+		assertSequenceEquals(new MediaFile[] { mediaFiles[1], mediaFiles[2], mediaFiles[0] }, playMode.getQueue());
 	}
 }
