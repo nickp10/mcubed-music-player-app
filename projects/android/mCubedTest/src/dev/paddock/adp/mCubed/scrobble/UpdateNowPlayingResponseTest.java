e51b0aa2a45e8ee09634077b6cfc36509f7b3f49
nickp10 <nickp10@hotmail.com>
2015-06-02 05:17:05 +0000

    Developer: Nick Paddock Bug/Feature #: N/A Description: Adding the request/response combination for updating the now playing media in Scrobble.

---
+++ projects/android/mCubedTest/src/dev/paddock/adp/mCubed/scrobble/UpdateNowPlayingResponseTest.java
@@ -0,0 +1,27 @@
+package dev.paddock.adp.mCubed.scrobble;
+
+import android.test.AndroidTestCase;
+import dev.paddock.adp.mCubed.ScrobbleResponseUtils;
+import dev.paddock.adp.mCubed.utilities.Utilities;
+
+public class UpdateNowPlayingResponseTest extends AndroidTestCase {
+	public void testSuccess() throws Exception {
+		UpdateNowPlayingResponse response = ScrobbleResponseUtils.parseResponse(UpdateNowPlayingResponse.class, "UpdateNowPlayingResponseSuccess.xml");
+		assertNotNull(response);
+		assertEquals("Eminem", response.getArtist());
+		assertEquals("Eminem", response.getAlbumArtist());
+		assertEquals("Recovery", response.getAlbum());
+		assertEquals("Not Afraid", response.getTrack());
+		assertTrue(Utilities.isNullOrEmpty(response.getIgnoredMessage()));
+	}
+
+	public void testFailMissingParameter() throws Exception {
+		try {
+			ScrobbleResponseUtils.parseResponse(MobileSessionResponse.class, "UpdateNowPlayingResponseMissingParameter.xml");
+			fail("Scrobble exception expected");
+		} catch (ScrobbleException e) {
+			assertEquals("6", e.getErrorCode());
+			assertNotNull(e.getErrorMessage());
+		}
+	}
+}
